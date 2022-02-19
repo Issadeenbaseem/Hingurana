a04d813e61f4a024e7fdc8bebb1c954642cce266
Issadeenbaseem <issadeenbaseem95@gmail.com>
2022-02-19 06:11:13 +0530

    2022/02/19

---
+++ components/Header.vue
@@ -0,0 +1,85 @@
+
+<template>
+<no-ssr>
+    <header>
+
+        <!-- Start Navigation -->
+        <nav class="navbar navbar-default navbar-sticky bootsnav">
+
+            <div class="container-full">
+
+                <!-- Start Atribute Navigation -->
+                <div class="attr-nav">
+                    <div class="call">
+                        <div class="icon">
+                            <i class="fas fa-phone"></i>
+                        </div>
+                        <div class="info">
+                            <span>Have any question?</span>
+                            <h5><a href="tel:+94 11 291 1934">(+94)11 291 1934</a></h5>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Atribute Navigation -->
+
+                <!-- Start Header Navigation -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
+                        <i class="fa fa-bars"></i>
+                    </button>
+                    <a class="navbar-brand" href="index.html">
+                        <img src="assets/img/logo-red.png" class="logo" alt="Logo">
+                    </a>
+                </div>
+                <!-- End Header Navigation -->
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="navbar-menu">
+                    <ul class="nav navbar-nav navbar-center" data-in="#" data-out="#">
+                        <li >
+                            <n-link to="/" class="dropdown-toggle active" data-toggle="dropdown" >Home</n-link>
+
+                        </li>
+                        <li>
+                            <NuxtLink to="about" class="dropdown-toggle " data-toggle="dropdown">About Us</NuxtLink>
+
+                        </li>
+                        <li >
+                            <NuxtLink to="company" class="dropdown-toggle " data-toggle="dropdown" >Companies</NuxtLink>
+
+                        </li>
+
+                        <li >
+                            <NuxtLink to="career"  class="dropdown-toggle" data-toggle="dropdown" >Careers</NuxtLink>
+
+                        </li>
+                        <li >
+                            <NuxtLink to="/contact" class="dropdown-toggle" data-toggle="dropdown" >Contact Us</NuxtLink>
+
+                        </li>
+                        <li >
+                            <NuxtLink to="media" class="dropdown-toggle" data-toggle="dropdown" >Media</NuxtLink>
+
+                        </li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div>
+
+        </nav>
+        <!-- End Navigation -->
+
+
+    </header>
+</no-ssr>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/media.vue
@@ -0,0 +1,49 @@
+
+<template>
+  <div>
+
+
+     
+
+    <modal name="my-modal">
+      <div class="modal-content">
+        <h2>It work's!</h2>
+
+        <button @click="$modal.hide('my-modal')">Close it</button>
+      </div>
+    </modal>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+
+
+};
+</script>
+
+<style>
+header,
+h2 {
+  margin-bottom: 20px;
+}
+.modal-content {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
+
+</template>
+
+<script>
+
+
+</script>
+
+<style>
+
+</style>
